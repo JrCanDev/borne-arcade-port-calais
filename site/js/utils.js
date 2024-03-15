d858b916fb94eadf5f5fa788507de12bd56f908a
Joshua Vandaële <joshua@vandaele.software>
2024-03-15 15:59:48 +0100

    feat(site): Page jeu

--- site/js/utils.js
+++ site/js/utils.js
@@ -5,3 +5,28 @@ function redirectToPage(e, url) {
 function stopPropagation(e) {
     e.stopPropagation();
 }
+
+function simulateKeyPress(keyCode) {
+    const event = new KeyboardEvent('keydown', {
+        keyCode: keyCode,
+        which: keyCode
+    });
+    document.dispatchEvent(event);
+}
+
+function emulateArrowClick(direction) {
+    switch (direction) {
+        case 'up':
+            simulateKeyPress(38); // ArrowUp
+            break;
+        case 'left':
+            simulateKeyPress(37); // ArrowLeft
+            break;
+        case 'down':
+            simulateKeyPress(40); // ArrowDown
+            break;
+        case 'right':
+            simulateKeyPress(39); // ArrowRight
+            break;
+    }
+}
