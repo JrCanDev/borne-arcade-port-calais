c4c98de480179bc2d76a3ce5891a04302af7002b
Joshua Vandaële <joshua@vandaele.software>
2024-03-18 10:01:31 +0100

    feat(site): Ajout de Snake

--- site/js/i18n.js
+++ site/js/i18n.js
@@ -1,27 +1,37 @@
-let locale = localStorage.getItem('locale') || "en";
+let locale = localStorage.getItem("locale") || "en";
 let translations = [];
 
 async function getTranslations(locale) {
-  const response = await fetch(`languages/${locale}.json`);
-  return await response.json();
+  let directory = '';
+  for (let i = 0; i <= 5; i++) {
+    try {
+      const response = await fetch(directory + `languages/${locale}.json`);
+      if (response.ok) {
+        return await response.json();
+      }
+    } catch (error) {
+      console.error(`Failed to fetch translations: ${error}`);
+    }
+    directory = "../" + directory;
+  }
+  throw new Error('Failed to fetch translations after multiple attempts');
 }
 
 async function setLocale(newLocale) {
   locale = newLocale;
-  localStorage.setItem('locale', locale);
+  localStorage.setItem("locale", locale);
   translations = await getTranslations(locale);
   translatePage();
 }
 
 function translateElement(element) {
   const key = element.getAttribute("data-i18n");
-  const field = element.getAttribute("data-i18n-field")
+  const field = element.getAttribute("data-i18n-field");
   const translation = translations[key];
-  if (field)
-  {
+  if (field) {
     element.setAttribute(field, translation);
   } else {
-      element.innerText = translation;
+    element.innerText = translation;
   }
 }
 
--- site/js/utils.js
+++ site/js/utils.js
@@ -13,20 +13,3 @@ function simulateKeyPress(keyCode) {
     });
     document.dispatchEvent(event);
 }
-
-function emulateArrowClick(direction) {
-    switch (direction) {
-        case 'up':
-            simulateKeyPress(38); // ArrowUp
-            break;
-        case 'left':
-            simulateKeyPress(37); // ArrowLeft
-            break;
-        case 'down':
-            simulateKeyPress(40); // ArrowDown
-            break;
-        case 'right':
-            simulateKeyPress(39); // ArrowRight
-            break;
-    }
-}
