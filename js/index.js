1791f6fea6aa477955192e4b89a70dd6f1c7e280
Joshua Vandaële <joshua@vandaele.software>
2024-04-22 15:06:10 +0200

    feat(site): Pacman bg animation

--- js/index.js
+++ js/index.js
@@ -60,7 +60,8 @@ class Puzzle extends BgAnimation {
     this.fullImage = null;
     this.isSolving = false; // Add this line
     setInterval(() => {
-      if (!this.isSolving) { // Add this line
+      if (!this.isSolving) {
+        // Add this line
         this.solve_random_piece();
       }
     }, this.timeToSolve);
@@ -69,7 +70,7 @@ class Puzzle extends BgAnimation {
   async createPuzzleImage() {
     const puzzleImage = await this.createImageFromBackground(BACKGROUND);
     this.puzzleImage = puzzleImage;
-  
+
     if (this.background) {
       this.background.imageMetadata.content = puzzleImage;
       this.background.adjustImagesToPuzzleHeight();
@@ -77,23 +78,23 @@ class Puzzle extends BgAnimation {
       this.background.redraw();
     }
   }
-  
+
   async createImageFromBackground(background) {
     const puzzleImage = new Image();
-    const canvas = document.createElement('canvas');
+    const canvas = document.createElement("canvas");
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
-    const context = canvas.getContext('2d');
-  
+    const context = canvas.getContext("2d");
+
     const response = await fetch(background);
     const blob = await response.blob();
     const imgBitmap = await createImageBitmap(blob);
-  
+
     context.drawImage(imgBitmap, 0, 0, WIDTH, HEIGHT);
     const dataUrl = canvas.toDataURL();
-  
+
     puzzleImage.src = dataUrl;
-  
+
     return puzzleImage;
   }
 
@@ -198,9 +199,9 @@ class Puzzle extends BgAnimation {
   }
 
   lerp(centralPosA, centralPosB, t) {
-    centralPosA.x = centralPosA.x + (centralPosB.x - centralPosA.x) * t
-    centralPosA.y = centralPosA.y + (centralPosB.y - centralPosA.y) * t
-    return centralPosA
+    centralPosA.x = centralPosA.x + (centralPosB.x - centralPosA.x) * t;
+    centralPosA.y = centralPosA.y + (centralPosB.y - centralPosA.y) * t;
+    return centralPosA;
   }
 
   solve_random_piece() {
@@ -211,7 +212,7 @@ class Puzzle extends BgAnimation {
       return;
     }
     let to_solve = this.solved_positions.pop();
-  
+
     let solved_position = to_solve.centralAnchor.copy();
     let current_position = to_solve.piece.centralAnchor.copy();
     let t = 0;
@@ -243,7 +244,10 @@ class Puzzle extends BgAnimation {
 class FlappyBird extends BgAnimation {
   constructor() {
     super();
-    this.elements = [document.getElementById("animated-background-flappy"), document.getElementById("animated-background-normal")];
+    this.elements = [
+      document.getElementById("animated-background-flappy"),
+      document.getElementById("animated-background-normal"),
+    ];
     this.birdElement = document.getElementById("flappy-bird");
     this.position = { x: 0, y: 0 };
     this.velocity = { x: 5, y: 0 };
@@ -251,23 +255,27 @@ class FlappyBird extends BgAnimation {
     this.flapPower = -10;
     this.intervalId = null;
     this.yGoal = HEIGHT / 2; // The y-coordinate of the center of the screen
-    this.flapSprites = [
-      "img/planeicons.png",
-      "img/planeicons-flap.png",
-    ]
+    this.flapSprites = ["img/planeicons.png", "img/planeicons-flap.png"];
     this.currentSpriteIndex = 0;
     this.flapIntervalId = null;
   }
 
   init() {
-    this.position = { x: this.birdElement.offsetLeft, y: Math.floor(Math.random() * HEIGHT) };
-    this.elements.forEach((element) => { element.style.opacity = 1 });
+    this.position = {
+      x: this.birdElement.offsetLeft,
+      y: Math.floor(Math.random() * HEIGHT),
+    };
+    this.elements.forEach((element) => {
+      element.style.opacity = 1;
+    });
     this.intervalId = setInterval(() => this.flap(), 20);
     this.flapIntervalId = setInterval(() => this.changeSprite(), 100);
   }
 
   destroy() {
-    this.elements.forEach((element) => { element.style.opacity = 0; });
+    this.elements.forEach((element) => {
+      element.style.opacity = 0;
+    });
     if (this.intervalId) {
       clearInterval(this.intervalId);
       this.intervalId = null;
@@ -290,11 +298,11 @@ class FlappyBird extends BgAnimation {
       this.velocity.y += this.gravity;
       this.position.y += this.velocity.y;
       this.position.x += this.velocity.x;
-  
+
       let rotation = Math.min(Math.max(this.velocity.y, -10), 10) * 3; // Adjust the multiplier as needed
-  
+
       this.birdElement.style.transform = `translate(${this.position.x}px, ${this.position.y}px) rotate(${rotation}deg)`;
-  
+
       if (this.position.y < 0 || this.position.y > this.yGoal) {
         this.velocity.y = this.flapPower;
       }
@@ -302,12 +310,287 @@ class FlappyBird extends BgAnimation {
   }
 
   changeSprite() {
-    this.currentSpriteIndex = (this.currentSpriteIndex + 1) % this.flapSprites.length;
+    this.currentSpriteIndex =
+      (this.currentSpriteIndex + 1) % this.flapSprites.length;
     this.birdElement.src = this.flapSprites[this.currentSpriteIndex];
   }
 }
 
-let animations = [new Normal(), new Puzzle(), new FlappyBird()];
+class Pacman extends BgAnimation {
+  constructor() {
+    super();
+    this.over = false;
+    this.elements = [
+      document.getElementById("animated-background-pacman"),
+      document.getElementById("animated-background-normal"),
+    ];
+    this.pacman = this.getPacmanCharacter();
+    this.ghost = this.getGhostCharacter();
+    this.characterMovementSpeed = 2;
+    this.pelletContainer = document.getElementById("pacman-pellets");
+    this.pellets = [];
+    this.pelletSize = 25;
+    this.superPelletSize = 50;
+    this.pelletGap = 10;
+    this.pacmanGhostGap = 1000;
+    this.paths = this.getPaths();
+    this.pacmanIntervalId = null;
+    this.isPacmanSuper = false;
+  }
+
+  getPacmanCharacter() {
+    return {
+      element: document.getElementById("pacman"),
+      position: { x: 0, y: 0 },
+      sprites: [
+        "jeux/pacman/img/pacman/1.png", // right
+        "jeux/pacman/img/pacman/2.png", // down
+        "jeux/pacman/img/pacman/3.png", // left
+        "jeux/pacman/img/pacman/4.png", // up
+      ],
+    };
+  }
+
+  getGhostCharacter() {
+    const normalSprites = [
+      "jeux/pacman/img/ghosts/ghost1Right.png",
+      "jeux/pacman/img/ghosts/ghost1Down.png",
+      "jeux/pacman/img/ghosts/ghost1Left.png",
+      "jeux/pacman/img/ghosts/ghost1Up.png",
+    ];
+    const scaredSprites = [
+      "jeux/pacman/img/ghosts/ghostAffraidRight.png",
+      "jeux/pacman/img/ghosts/ghostAffraidDown.png",
+      "jeux/pacman/img/ghosts/ghostAffraidLeft.png",
+      "jeux/pacman/img/ghosts/ghostAffraidUp.png",
+    ];
+    return {
+      element: document.getElementById("pacman-ghost"),
+      position: { x: 0, y: 0 },
+      normalSprites: normalSprites,
+      scaredSprites: scaredSprites,
+      sprites: normalSprites,
+    };
+  }
+
+  getPaths() {
+    return [
+      { x: -this.pelletSize * 2, y: HEIGHT / 2 }, // Start offscreen
+      { x: WIDTH / 4, y: null }, // null means the y-coordinate is not changed, so we move horizontally
+      { x: null, y: HEIGHT / 3 }, // null means the x-coordinate is not changed, so we move vertically
+      { x: WIDTH / 1.25, y: null },
+      { x: null, y: HEIGHT / 1.5 },
+      { x: WIDTH / 1.1, y: null },
+      { x: null, y: HEIGHT / 5 },
+    ];
+  }
+
+  init() {
+    this.elements.forEach((element) => {
+      element.style.opacity = 1;
+    });
+    this.pacman.position = { x: this.paths[0].x, y: this.paths[0].y };
+    this.ghost.position = { x: this.paths[0].x, y: this.paths[0].y };
+    this.updateCharacter(this.pacman);
+    this.updateCharacter(this.ghost);
+    this.generatePellets();
+    this.pacmanIntervalId = setInterval(
+      () => this.playAnimation(this.pacman),
+      10
+    );
+    setTimeout(() => {
+      this.ghostIntervalId = setInterval(
+        () => this.playAnimation(this.ghost),
+        10
+      );
+    }, this.pacmanGhostGap);
+  }
+
+  generatePellets() {
+    let lastPath = this.paths[0];
+    this.paths.forEach((path) => {
+      if (path.x === null) {
+        path.x = lastPath.x;
+      }
+      if (path.y === null) {
+        path.y = lastPath.y;
+      }
+      let distance = Math.sqrt(
+        Math.pow(path.x - lastPath.x, 2) + Math.pow(path.y - lastPath.y, 2)
+      );
+      let pellets = Math.floor(distance / (this.pelletSize + this.pelletGap));
+      for (let i = 0; i < pellets; i++) {
+        let pellet = document.createElement("img");
+        pellet.src = "jeux/pacman/img/bubbles/bubble.png";
+        pellet.style.position = "absolute";
+        pellet.className = "pellet";
+        pellet.style.width = `${this.pelletSize}px`;
+        pellet.style.height = `${this.pelletSize}px`;
+        pellet.style.left = `${
+          lastPath.x +
+          ((path.x - lastPath.x) / pellets) * i -
+          this.pelletSize / 2
+        }px`;
+        pellet.style.top = `${
+          lastPath.y +
+          ((path.y - lastPath.y) / pellets) * i -
+          this.pelletSize / 2
+        }px`;
+        this.pelletContainer.appendChild(pellet);
+        this.pellets.push(pellet);
+      }
+      lastPath = path;
+    });
+
+    // Generate the last pellet which is a special one
+    let pellet = document.createElement("img");
+    pellet.src = "jeux/pacman/img/bubbles/superbubble.png";
+    pellet.style.position = "absolute";
+    pellet.className = "pellet";
+    pellet.style.width = `${this.superPelletSize}px`;
+    pellet.style.height = `${this.superPelletSize}px`;
+    pellet.style.left = `${lastPath.x - this.superPelletSize / 2}px`;
+    pellet.style.top = `${lastPath.y - this.superPelletSize / 2}px`;
+    this.pelletContainer.appendChild(pellet);
+    this.pellets.push(pellet);
+  }
+
+  updateCharacter(character, direction) {
+    let characterSize = character.element.width;
+    character.element.style.left = `${
+      character.position.x - characterSize / 2
+    }px`;
+    character.element.style.top = `${
+      character.position.y - characterSize / 2
+    }px`;
+    if (direction !== undefined) {
+      character.element.src = character.sprites[direction];
+    }
+  }
+
+  calculateDirection(currentPath, nextPath) {
+    let directionX = nextPath.x - currentPath.x;
+    let directionY = nextPath.y - currentPath.y;
+  
+    // Normalize the direction
+    let distance = Math.sqrt(directionX * directionX + directionY * directionY);
+    directionX /= distance;
+    directionY /= distance;
+  
+    return { directionX, directionY };
+  }
+  
+  updatePositions(character, directionX, directionY) {
+    // Update the positions
+    character.position.x += directionX * this.characterMovementSpeed;
+    character.position.y += directionY * this.characterMovementSpeed;
+  }
+  
+  updateSprites(character, directionX, directionY) {
+    // Update the sprites
+    let spriteDirection = 0;
+    if (directionX < 0) {
+      spriteDirection = 2;
+    } else if (directionY > 0) {
+      spriteDirection = 1;
+    } else if (directionY < 0) {
+      spriteDirection = 3;
+    }
+  
+    // Update the elements on the screen
+    this.updateCharacter(character, spriteDirection);
+  }
+  
+  checkNextPath(character, nextPath) {
+    // If we've reached the next path, move on to the next one
+    if (
+      Math.abs(character.position.x - nextPath.x) < this.characterMovementSpeed &&
+      Math.abs(character.position.y - nextPath.y) < this.characterMovementSpeed
+    ) {
+      character.currentPathIndex += this.isPacmanSuper ? -1 : 1;
+    }
+  }
+  
+  checkPelletCollision() {
+    this.pellets = this.pellets.filter((pellet) => {
+      const pelletRect = pellet.getBoundingClientRect();
+      const pacmanRect = this.pacman.element.getBoundingClientRect();
+  
+      const offset = this.pellets.length > 1 ? this.pelletSize : this.superPelletSize;
+  
+      const collision = !(
+        pelletRect.right < pacmanRect.left + offset ||
+        pelletRect.left > pacmanRect.right - offset ||
+        pelletRect.bottom < pacmanRect.top + offset ||
+        pelletRect.top > pacmanRect.bottom - offset
+      );
+  
+      if (collision) {
+        this.pelletContainer.removeChild(pellet);
+      }
+  
+      return !collision;
+    });
+  }
+  
+  playAnimation(character) {
+    // Assuming we have a currentPathIndex to keep track of the current path
+    if (character.currentPathIndex === undefined) {
+      character.currentPathIndex = this.isPacmanSuper
+        ? this.paths.length - 1
+        : 0;
+    }
+  
+    let currentPath = this.paths[character.currentPathIndex];
+    let nextPath = this.isPacmanSuper
+      ? this.paths[character.currentPathIndex - 1]
+      : this.paths[character.currentPathIndex + 1];
+  
+    // If there's no next path, we've reached the end of the animation
+    if (!nextPath) {
+      if (this.isPacmanSuper) {
+        this.over = true;
+        return;
+      }
+      this.isPacmanSuper = true;
+      this.ghost.sprites = this.ghost.scaredSprites;
+      this.pacman.currentPathIndex = this.paths.length - 1;
+      this.ghost.currentPathIndex = this.paths.length - 1;
+      return;
+    }
+  
+    const { directionX, directionY } = this.calculateDirection(currentPath, nextPath);
+    this.updatePositions(character, directionX, directionY);
+    this.updateSprites(character, directionX, directionY);
+    this.checkNextPath(character, nextPath);
+  
+    // If the character is Pacman, check for collisions with pellets
+    if (character === this.pacman) {
+      this.checkPelletCollision();
+    }
+  }
+
+  destroy() {
+    this.elements.forEach((element) => {
+      element.style.opacity = 0;
+    });
+    this.pellets.forEach((pellet) => {
+      this.pelletContainer.removeChild(pellet);
+    });
+    this.pellets = [];
+    if (this.pacmanIntervalId) {
+      clearInterval(this.pacmanIntervalId);
+      this.pacmanIntervalId = null;
+    }
+    this.ghost.sprites = this.ghost.normalSprites;
+  }
+
+  isOver() {
+    return this.over;
+  }
+}
+// new Normal(), new Puzzle(), new FlappyBird(),
+let animations = [new Pacman()];
 
 animations.forEach((animation) => {
   animation.destroy();
