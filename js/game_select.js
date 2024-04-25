ef8c3ddb76cd48a03f282dabec18c439944440cb
Joshua Vandaële <joshua@vandaele.software>
2024-04-25 14:59:16 +0200

    feat(site): Retirer le second indicateur redondant pour le scroll

--- js/game_select.js
+++ js/game_select.js
@@ -3,10 +3,8 @@ window.onload = function() {
     document.querySelector('main').addEventListener('scroll', function() {
         clearTimeout(timeout);
         document.getElementById('swipe-animation').style.opacity = 0;
-        document.getElementById('scroll-arrow').style.opacity = 0;
         timeout = setTimeout(function() {
             document.getElementById('swipe-animation').style.opacity = 1;
-            document.getElementById('scroll-arrow').style.opacity = 1;
         }, 5000);
     });
 }
