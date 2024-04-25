520a576fcb23290a8153eb7de28178357017f04d
Joshua Vandaële <joshua@vandaele.software>
2024-04-25 15:03:34 +0200

    feat(site): Commencer avec le swip hint en off

--- js/game_select.js
+++ js/game_select.js
@@ -1,10 +1,21 @@
+const SWIPE_HINT_COOLDOWN = 5000;
+
+function setSwipeAnimationOpacity(opacity, delay = 0) {
+    if (delay === 0) {
+        document.getElementById('swipe-animation').style.opacity = opacity;
+        return -1;
+    }
+    return setTimeout(function() {
+        document.getElementById('swipe-animation').style.opacity = opacity;
+    }, delay);
+}
+
 window.onload = function() {
-    let timeout;
+    let timeout = setSwipeAnimationOpacity(1, SWIPE_HINT_COOLDOWN);
+
     document.querySelector('main').addEventListener('scroll', function() {
         clearTimeout(timeout);
-        document.getElementById('swipe-animation').style.opacity = 0;
-        timeout = setTimeout(function() {
-            document.getElementById('swipe-animation').style.opacity = 1;
-        }, 5000);
+        setSwipeAnimationOpacity(0);
+        timeout = setSwipeAnimationOpacity(1, SWIPE_HINT_COOLDOWN);
     });
 }
