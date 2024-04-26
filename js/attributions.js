b00df26ab32123e03ac1a9fc259e5df0986537f2
Joshua Vandaële <joshua@vandaele.software>
2024-04-26 11:37:55 +0200

    refactor(site): Séparation de HTML et JS

---
+++ js/attributions.js
@@ -0,0 +1,14 @@
+let hiddenReset = document.getElementById("hidden-reset");
+let hiddenResetClicks = 0;
+hiddenReset.addEventListener("click", () => {
+    hiddenResetClicks++;
+    if (hiddenResetClicks == 10) {
+        if (confirm("Êtes-vous sûr de vouloir effacer les données locales ?")) {
+            localStorage.clear();
+            alert("Données locales effacées.");
+            window.history.back();
+        } else {
+            hiddenResetClicks = 0;
+        }
+    }
+});
--- js/game_select.js
+++ js/game_select.js
@@ -19,3 +19,27 @@ window.onload = function() {
         timeout = setSwipeAnimationOpacity(1, SWIPE_HINT_COOLDOWN);
     });
 }
+
+function onLocaleChange() {
+    document.querySelector(`img[src="img/${locale}-flag.svg"]`).id = "selected-language";
+    console.log("loaded")
+}
+
+// Change the id "#selected-language" to the new selected language when an img in the header is clicked
+document.querySelectorAll("img").forEach(img => {
+    img.addEventListener("click", function () {
+        document.querySelector("#selected-language").removeAttribute("id");
+        document.querySelector(`img[src="img/${locale}-flag.svg"]`).id = "selected-language";
+    });
+});
+
+function chooseBetweenGames(event, game1, game2) {
+    event.stopPropagation();
+
+    var element = document.getElementById(game1 + "-" + game2);
+    if (!element) {
+        element = document.getElementById(game2 + "-" + game1);
+    }
+
+    element.style.display = "block";
+}
