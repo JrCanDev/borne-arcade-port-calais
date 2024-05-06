565442eb90f2a7bb47f84c102fa6cdfd7c248391
Joshua Vandaële <joshua@vandaele.software>
2024-05-06 11:13:00 +0200

    feat(site): Ajout d'effets sonores pour les attributions

--- js/attributions.js
+++ js/attributions.js
@@ -11,11 +11,35 @@ function clearLocalData() {
 window.onload = function () {
     const hiddenReset = document.getElementById("hidden-reset");
     let hiddenResetClicks = 0;
+    let currentAudio = null;
+    let currentAudioTimeout = null;
 
     hiddenReset.addEventListener("click", () => {
         hiddenResetClicks++;
         if (hiddenResetClicks == 10) {
             clearLocalData();
         }
+    });
+    
+    document.querySelector('.audio').addEventListener('click', function playAudio() {
+        let audioSrc = this.getAttribute('data-src');
+        if (!audioSrc) {
+            console.error('No audio source found');
+            return;
+        }
+
+        if (currentAudio || currentAudioTimeout) {
+            clearTimeout(currentAudioTimeout);
+            currentAudio.pause();
+            currentAudio = null;
+        }
+    
+        currentAudio = new Audio(audioSrc);
+        currentAudio.play().catch(error => console.error(`Error playing audio: ${error}`));
+    
+        currentAudioTimeout = setTimeout(function() {
+            currentAudio.pause();
+            currentAudio = null;
+        }, 5000);
     });
 }
