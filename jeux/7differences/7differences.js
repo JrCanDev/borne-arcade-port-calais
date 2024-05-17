46076a7962812c5778d7438765ff4d1422c1e5e9
Joshua Vandaële <joshua@vandaele.software>
2024-05-17 11:02:10 +0200

    fix(7difference): On ne peut plus deviner avant que l'image change

--- jeux/7differences/7differences.js
+++ jeux/7differences/7differences.js
@@ -1,65 +1,71 @@
 DIFFERENCE_COUNT = 7;
 COUNTDOWN_SECONDS = 5;
 
-window.onload = async function() {
-    let ORIGINAL = await findImageWithAvailableExtension("img/original");
-    let MODIFIED = await findImageWithAvailableExtension("img/modified");
-    let ACCURACY_COUNTER = document.getElementById("accuracy");
-    let FOUND_COUNTER = document.getElementById("found");
-    let SHOWN_IMAGE = document.getElementById("diff-img");
-    let COUNTDOWN_COUNTER = document.getElementById("countdown");
-    let guesses = 0;
-    let found = 0;
+window.onload = async function () {
+  let ORIGINAL = await findImageWithAvailableExtension("img/original");
+  let MODIFIED = await findImageWithAvailableExtension("img/modified");
+  let ACCURACY_COUNTER = document.getElementById("accuracy");
+  let FOUND_COUNTER = document.getElementById("found");
+  let SHOWN_IMAGE = document.getElementById("diff-img");
+  let COUNTDOWN_COUNTER = document.getElementById("countdown");
+  let guesses = 0;
+  let found = 0;
 
-    SHOWN_IMAGE.src = ORIGINAL;
-    
-    let countdown = COUNTDOWN_SECONDS;
-    let countdownInterval = setInterval(function() {
-        COUNTDOWN_COUNTER.innerHTML = getTranslation("game.7differences.memorise") + "\n" + countdown;
-        countdown--;
-        if (countdown < 0) {
-            SHOWN_IMAGE.src = MODIFIED;
-            SHOWN_IMAGE.classList.add('slide-in');
-            clearInterval(countdownInterval);
-            COUNTDOWN_COUNTER.innerHTML = "";
-        }
-    }, 1000);
+  SHOWN_IMAGE.src = ORIGINAL;
 
-
-    function updateAccuracyCounter() {
-        ACCURACY_COUNTER.innerHTML = Math.round((found / guesses) * 100) + "%";
+  let countdown = COUNTDOWN_SECONDS;
+  let countdownInterval = setInterval(function () {
+    COUNTDOWN_COUNTER.innerHTML =
+      getTranslation("game.7differences.memorise") + "\n" + countdown;
+    countdown--;
+    if (countdown < 0) {
+      onMemorisationEnd();
     }
+  }, 1000);
 
-    function updateFoundCounter() {
-        FOUND_COUNTER.innerHTML = found;
-    }
+  function updateAccuracyCounter() {
+    ACCURACY_COUNTER.innerHTML = Math.round((found / guesses) * 100) + "%";
+  }
 
-    document.getElementById("game-container").addEventListener("touchstart", function() {
-        guesses += 1;
-        setTimeout(function() {
-            updateAccuracyCounter();
-        }, 100);
-    });
+  function updateFoundCounter() {
+    FOUND_COUNTER.innerHTML = found;
+  }
 
-    let differences = []
+  function onMemorisationEnd() {
+    SHOWN_IMAGE.src = MODIFIED;
+    SHOWN_IMAGE.classList.add("slide-in");
+    clearInterval(countdownInterval);
+    COUNTDOWN_COUNTER.innerHTML = "";
+
+    let differences = [];
     for (let i = 0; i < DIFFERENCE_COUNT; i++) {
-        differences.push(document.getElementById('difference' + (i + 1)));
+      differences.push(document.getElementById("difference" + (i + 1)));
     }
 
     function checkDifference(id) {
-        let difference = differences[id - 1];
-        if (difference.style.opacity != '1') {
-            found++;
-            updateAccuracyCounter();
-            updateFoundCounter();
-            difference.style.opacity = '1';
-        }
+      let difference = differences[id - 1];
+      if (difference.style.opacity != "1") {
+        found++;
+        updateAccuracyCounter();
+        updateFoundCounter();
+        difference.style.opacity = "1";
+      }
 
-        if (found == DIFFERENCE_COUNT) {
-            showGameOver();
-        }
+      if (found == DIFFERENCE_COUNT) {
+        showGameOver();
+      }
     }
 
     // Attach the checkDifference function to the global scope so it can be accessed from the HTML
     window.checkDifference = checkDifference;
-}
+
+    document
+      .getElementById("game-container")
+      .addEventListener("touchstart", function () {
+        guesses += 1;
+        setTimeout(function () {
+          updateAccuracyCounter();
+        }, 100);
+      });
+  }
+};
