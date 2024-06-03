78c001da4c03cef9f15e670de90c478fb72f1210
Joshua Vandaële <joshua@vandaele.software>
2024-06-03 10:12:25 +0200

    feat(maze): Ajout d'objectifs

--- jeux/maze/maze.js
+++ jeux/maze/maze.js
@@ -2,17 +2,21 @@
 // Black = wall
 // Red = start
 // Green = goal
+// Yellow = dangerous
 // Other = path
 // The path image is invisible to the player, but the game uses it to determine the maze layout.
-
+const GOALS = 6;
+const DEBUG = false;
 /**
  * Represents a maze game where you can drag the character to the goal.
  */
 class Maze {
   constructor(bg_image, maze_image, onGoalReached) {
+    if (DEBUG) {
+      bg_image = maze_image;
+    }
     this.gameOver = false;
     this.position = { x: 0, y: 0 };
-    this.currentGoal = 0;
     this.gameContainer = document.getElementById("game-container");
     this.dimensions = this.gameContainer.getBoundingClientRect();
     this.characterElement = document.getElementById("character");
@@ -42,17 +46,29 @@ class Maze {
     );
   }
 
-  loadImages(bg_image, maze_image) {
-    this.maze_path_image = new Image();
-    this.maze_path_image.src = maze_image;
-    this.maze_path_image.onload = () => {
-      this.createMazeCanvas();
-      this.findStartPosition();
-    };
-  
-    this.maze_bg_image = new Image();
-    this.maze_bg_image.src = bg_image;
-    this.maze_bg_image.onload = this.setBackgroundImage.bind(this);
+  removeEventListeners() {
+    const startEvents = ["mousedown", "touchstart"];
+    const endEvents = ["mouseup", "touchend"];
+    const moveEvents = ["mousemove", "touchmove"];
+
+    startEvents.forEach((event) =>
+      this.characterElement.removeEventListener(event, this.dragStart, false)
+    );
+    endEvents.forEach((event) =>
+      document.removeEventListener(event, this.dragEnd, false)
+    );
+    moveEvents.forEach((event) =>
+      document.removeEventListener(event, this.drag, false)
+    );
+  }
+
+  async loadImages(bg_image, maze_image) {
+    this.maze_path_image = await getImageWithAvailableExtension(maze_image);
+    this.createMazeCanvas();
+    this.findStartPosition();
+
+    this.maze_bg_image = await getImageWithAvailableExtension(bg_image);
+    this.setBackgroundImage();
   }
 
   findStartPosition() {
@@ -62,29 +78,30 @@ class Maze {
       this.mazeCanvas.width,
       this.mazeCanvas.height
     ).data;
-  
+
     let startPositions = [];
-    
+
     for (let i = 0; i < imageData.length; i += 4) {
       if (this.isStart([imageData[i], imageData[i + 1], imageData[i + 2]])) {
         startPositions.push({
           left: (i / 4) % this.mazeCanvas.width,
-          top: Math.floor((i / 4) / this.mazeCanvas.width)
+          top: Math.floor(i / 4 / this.mazeCanvas.width),
         });
       }
     }
-  
+
     if (startPositions.length > 0) {
-      let minLeft = Math.min(...startPositions.map(p => p.left));
-      let maxLeft = Math.max(...startPositions.map(p => p.left));
-      let minTop = Math.min(...startPositions.map(p => p.top));
-      let maxTop = Math.max(...startPositions.map(p => p.top));
-  
+      let minLeft = Math.min(...startPositions.map((p) => p.left));
+      let maxLeft = Math.max(...startPositions.map((p) => p.left));
+      let minTop = Math.min(...startPositions.map((p) => p.top));
+      let maxTop = Math.max(...startPositions.map((p) => p.top));
+
       let startPosition = {
-        left: minLeft + (maxLeft - minLeft) / 2 - this.characterElement.width / 2,
-        top: minTop + (maxTop - minTop) / 2 - this.characterElement.height / 2
-      }
-  
+        left:
+          minLeft + (maxLeft - minLeft) / 2 - this.characterElement.width / 2,
+        top: minTop + (maxTop - minTop) / 2 - this.characterElement.height / 2,
+      };
+
       this.updateCharacterPosition(startPosition);
     }
   }
@@ -122,20 +139,20 @@ class Maze {
 
   drag(event) {
     if (!this.dragging || this.gameOver) return;
-  
+
     event.preventDefault();
-  
+
     let currentX = event.clientX || event.touches[0].clientX;
     let currentY = event.clientY || event.touches[0].clientY;
-  
+
     let newPosition = this.calculateNewPosition(currentX, currentY);
     newPosition = this.adjustForContainer(newPosition);
-  
+
     if (!this.isOnPath(newPosition)) {
       this.dragging = false;
       return;
     }
-  
+
     this.updateCharacterPosition(newPosition);
 
     // Update the initial position for the next movement
@@ -144,82 +161,77 @@ class Maze {
 
     if (this.isOnGoal(newPosition)) {
       this.onGoalReached();
+      this.removeEventListeners();
       this.onGoalReached = () => {};
       this.gameOver = true;
     }
+
+    if (this.isOnDangerous(newPosition)) {
+      this.dragging = false;
+      this.findStartPosition();
+    }
   }
-  
+
   calculateNewPosition(currentX, currentY) {
     let diffX = currentX - this.initialX;
     let diffY = currentY - this.initialY;
-  
+
     let newLeft = this.characterElement.offsetLeft + diffX;
     let newTop = this.characterElement.offsetTop + diffY;
-  
+
     return { left: newLeft, top: newTop };
   }
-  
+
   adjustForContainer(position) {
     let containerRect = this.gameContainer.getBoundingClientRect();
     position.left -= containerRect.left;
     position.top -= containerRect.top;
-  
+
     return position;
   }
-  
+
   getPixelData(position) {
     const characterRect = this.characterElement.getBoundingClientRect();
+    const halfWidth = characterRect.width / 2;
+    const halfHeight = characterRect.height / 2;
+
+    const getPixel = (x, y) => this.mazeContext.getImageData(x, y, 1, 1).data;
+
     return {
-      topLeftPixelData: this.mazeContext.getImageData(
-        position.left,
-        position.top,
-        1,
-        1
-      ).data,
-      topRightPixelData: this.mazeContext.getImageData(
-        position.left + characterRect.width,
-        position.top,
-        1,
-        1
-      ).data,
-      bottomLeftPixelData: this.mazeContext.getImageData(
-        position.left,
-        position.top + characterRect.height,
-        1,
-        1
-      ).data,
-      bottomRightPixelData: this.mazeContext.getImageData(
-        position.left + characterRect.width,
-        position.top + characterRect.height,
-        1,
-        1
-      ).data,
+        topLeftPixelData: getPixel(position.left, position.top),
+        topCenterPixelData: getPixel(position.left + halfWidth, position.top),
+        topRightPixelData: getPixel(position.left + characterRect.width, position.top),
+        leftCenterPixelData: getPixel(position.left, position.top + halfHeight),
+        rightCenterPixelData: getPixel(position.left + characterRect.width, position.top + halfHeight),
+        bottomLeftPixelData: getPixel(position.left, position.top + characterRect.height),
+        bottomCenterPixelData: getPixel(position.left + halfWidth, position.top + characterRect.height),
+        bottomRightPixelData: getPixel(position.left + characterRect.width, position.top + characterRect.height),
+        centerPixelData: getPixel(position.left + halfWidth, position.top + halfHeight),
     };
+}
+
+  checkPosition(position, callback) {
+    const pixelData = this.getPixelData(position);
+
+    return Object.values(pixelData).some(callback);
   }
 
   isOnPath(position) {
-    const { topLeftPixelData, topRightPixelData, bottomLeftPixelData, bottomRightPixelData } = this.getPixelData(position);
-    return (
-      !this.isWall(topLeftPixelData) &&
-      !this.isWall(topRightPixelData) &&
-      !this.isWall(bottomLeftPixelData) &&
-      !this.isWall(bottomRightPixelData)
-    );
+    return this.checkPosition(position, this.isWall) === false;
   }
 
   isOnGoal(position) {
-    const { topLeftPixelData, topRightPixelData, bottomLeftPixelData, bottomRightPixelData } = this.getPixelData(position);
-    return (
-      this.isGoal(topLeftPixelData) ||
-      this.isGoal(topRightPixelData) ||
-      this.isGoal(bottomLeftPixelData) ||
-      this.isGoal(bottomRightPixelData)
-    );
+    return this.checkPosition(position, this.isGoal);
   }
-  
+
+  isOnDangerous(position) {
+    return this.checkPosition(position, this.isDangerous);
+  }
+
   updateCharacterPosition(position) {
     let containerRect = this.gameContainer.getBoundingClientRect();
-    this.characterElement.style.left = position.left + containerRect.left + "px";
+    this.characterElement.style.left =
+      position.left + containerRect.left + "px";
     this.characterElement.style.top = position.top + containerRect.top + "px";
   }
 
@@ -237,19 +249,34 @@ class Maze {
     // black
     return pixelData[0] === 0 && pixelData[1] === 0 && pixelData[2] === 0;
   }
+
+  isDangerous(pixelData) {
+    // yellow
+    return pixelData[0] >= 128 && pixelData[1] >= 128 && pixelData[2] === 0;
+  }
 }
 
 window.onload = async function () {
-  window.BACKGROUND_IMAGE = await findImageWithAvailableExtension("img/bg");
-  window.PATH_IMAGE = await findImageWithAvailableExtension("img/path");
-  window.CHARACTER_IMAGE = await findImageWithAvailableExtension("img/character");
-  window.DEBUG = false;
+  let backgroundImages = [];
+  let pathImages = [];
 
-  function onGoalReached() {
-    showGameOver()
+  for (let i = 0; i < GOALS + 1; i++) {
+    backgroundImages.push(`img/bg${i}`);
+    pathImages.push(`img/path${i}`);
   }
 
-  let maze = new Maze(DEBUG ? PATH_IMAGE : BACKGROUND_IMAGE, PATH_IMAGE, onGoalReached);
+  let currentGoal = 0;
 
-  window.maze = maze;
+  function onGoalReached() {
+    currentGoal++;
+    new Maze(
+      backgroundImages[currentGoal],
+      pathImages[currentGoal],
+      onGoalReached
+    );
+    if (currentGoal >= 6) {
+      showGameOver();
+    }
+  }
+  new Maze("img/bg0", "img/path0", onGoalReached);
 };
